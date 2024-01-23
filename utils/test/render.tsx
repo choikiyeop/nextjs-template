56af0d759ec2c99947e91b7673d9f9ac98c3ef32
choikiyeop <cky9709@naver.com>
2024-01-23 16:19:04 +0900

    test 환경에서 react-query 설정

--- utils/test/render.tsx
+++ utils/test/render.tsx
@@ -1,14 +1,21 @@
-import { render } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import { ReactElement } from "react";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { ReactElement } from 'react';
 
-export default async function getUserEventFromComponent(
-  component: ReactElement
-) {
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
+
+export default async function getUserEventFromComponent(component: ReactElement) {
   const user = userEvent.setup();
 
   return {
     user,
-    ...render(component),
+    ...render(<QueryClientProvider client={queryClient}>{component}</QueryClientProvider>),
   };
 }
