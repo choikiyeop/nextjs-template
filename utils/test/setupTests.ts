44a919395fdeb02b436b8ee636cd4d048dfba72f
choikiyeop <cky9709@naver.com>
2024-01-23 16:19:45 +0900

    test환경 msw 설정

--- utils/test/setupTests.ts
+++ utils/test/setupTests.ts
@@ -1,11 +1,18 @@
+import { server } from '@/__mocks__/server';
 import '@testing-library/jest-dom';
-import { afterAll, afterEach, vi } from 'vitest';
+import { afterAll, afterEach, beforeAll, vi } from 'vitest';
+
+beforeAll(() => {
+  server.listen();
+});
 
 afterEach(() => {
+  server.resetHandlers();
   vi.clearAllMocks();
 });
 
 afterAll(() => {
+  server.close();
   vi.resetAllMocks();
 });
 
